2762a53ebb21f2944ce0e6ec10189c78d19fc97e
cpoder <sag@DESKTOP-COGVHIJ.localdomain>
2020-12-09 15:08:30 +0100

    .gitignore working

--- cockpit-clustered-map/dist/apps/cockpit-clustered-map/vendors~branding.1c34f84ebb8fea65755f.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{3255:function(n,w,o){}}]);
--- cockpit-clustered-map/dist/apps/cockpit-clustered-map/vendors~branding.9beff6d27b48c3aa1e7b.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{3254:function(n,w,o){}}]);
